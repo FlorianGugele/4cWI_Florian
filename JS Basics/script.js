6962dd62e0900c508ea115cc296239217f401a3e
Florian <florian.gugele@gmail.com>
2023-10-05 14:11:41 +0200

    10. Funktionen

--- JS Basics/script.js
+++ JS Basics/script.js
@@ -4,3 +4,24 @@ function add(a,b){
 
 let addResult = add(8,33);
 console.log(addResult);
+
+function subtract(){
+    return a-b;
+}
+
+let subResult = subtract(32-17);
+console.log(subResult);
+
+function multiply(){
+    return a*b;
+}
+
+let multResult = multiply(23,16);
+console.log(multResult);
+
+function supercalc(){
+    return ((a+b) /2*a)
+}
+
+let supercalcResult = supercalc(17,36);
+console.log(supercalcResult);
