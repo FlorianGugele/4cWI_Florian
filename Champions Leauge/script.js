6afb01bc6f5b2126f6e09cf67b274292cfdf8cc7
Florian <florian.gugele@gmail.com>
2023-11-23 14:08:25 +0100

    API Angefangen

--- Champions Leauge/script.js
+++ Champions Leauge/script.js
@@ -1,12 +1,25 @@
-let scrollPosition = 0;
+async function checkTeamGames(teamid) {
+    try {
+        //const response = await fetch("https://api.openligadb.de/getmatchesbyteamid/"+teamid+"/1/12");
+        const response = await fetch("https://api.openligadb.de/getavailableleagues");
+        const contentType = response.headers.get("content-type");
+        if (!contentType || !contentType.includes("application/json")) {
+            throw new TypeError("Oops, we haven't got JSON!");
+        }
+        const data = await response.json();
 
-    function scrollRight() {
-        scrollPosition -= 1;
-        updateScroll();
-    }
+            data.forEach((element, index) => {
+                //var div = document.getElementById('gamesplay');
+                //div.innerHTML += element.leagueName + "("+element.matchDateTime+") "+ element.team1.teamName + " vs. "+element.team2.teamName +"<br>";
+                console.log(element.teamName)
+            });
 
-    function updateScroll() {
-        const container = document.querySelector('.flex.items-center');
-        const scrollValue = `${scrollPosition * 100}%`;
-        container.style.transform = `translateX(${scrollValue})`;
+        // process your data further
+    } catch (error) {
+        console.error("Error:", error);
     }
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+    checkTeamGames("")
+})
