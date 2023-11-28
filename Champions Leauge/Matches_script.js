2f1cd261d16afc77c363471b9bdfccd769986bed
Florian <florian.gugele@gmail.com>
2023-11-28 11:10:54 +0100

    API Matches

--- Champions Leauge/Matches_script.js
+++ Champions Leauge/Matches_script.js
@@ -1,46 +1,49 @@
-async function checkLeauge(teamid) {
+function checkGames(teamid) {
     try {
-        const response = await fetch("https://api.openligadb.de/getavailableleagues");
+        const response = () => {
+            fetch("https://api.openligadb.de/getmatchdata/champion1/2023").then((result) => {
+              result.json().then((data) => {
+                fillTable(data);
+              });
+            });
+          };
         const contentType = response.headers.get("content-type");
         if (!contentType || !contentType.includes("application/json")) {
             throw new TypeError("Oops, we haven't got JSON!");
         }
         const data = await response.json();
         console.log(data);
-        data.forEach((element, index) => {
-            console.log(element.leagueShortcut)
+        let html = "";
+        data.forEach((match, index) => {
+        '<div class="flex flex-col items-center bg-blue-500 m-2 h-[80%] w-[20%] mb-10 mr-10"><div><p class="text-white text-center font-bold text-xs">'
+        + match.matchDateTime + 
+        '</div><div><p class="text-white text-center font-bold text-xs">Gruppe ' 
+        + match.group.groupOrderID +
+        '</p></div><div class="flex items-center justify-between w-full"><div class="h-[3vh] w-[3vw] ml-[17%]"><a href="Teams.html"><img src="'
+        + match.team1.teamIconUrl +
+        '"></a></div><div class="text-4xl"><p class="text-white font-bold">:</p></div><div class="h-[3vh] w-[3vw] mr-[17%] mb-8"><a href="Teams.html"><img src="' 
+        + match.team2.teamIconUrl +
+        '"></a></div></div><div class="flex items-center justify-between w-full"><div class="text-center ml-[20%]"><p class="text-white align-text-bottom">'
+        + match.team1.teamName +
+        '</p></div><div class="text-center mr-[20%]"><p class="text-white align-text-bottom">'
+        + match.team2.teamName +
+        '</p></div></div></div>'
+
+            console.log(match.team1.teamName)
         });
+        document.getElementById("match").innerHTML = html;
+        console.log(html);
 
         // process your data further
     } catch (error) {
         console.error("Error:", error);
     }
+
+    
 }
 
-async function checkGames(teamid) {
-    try {
-        const response = await fetch("https://api.openligadb.de/getmatchdata/{champion1}/{2023}");
-        const contentType = response.headers.get("content-type");
-        if (!contentType || !contentType.includes("application/json")) {
-            throw new TypeError("Oops, we haven't got JSON!");
-        }
-        const data = await response.json();
-        console.log(data);
-        data.forEach((element, index) => {
-            //var div = document.getElementById('gamesplay');
-            //div.innerHTML += element.leagueName + "("+element.matchDateTime+") "+ element.team1.teamName + " vs. "+element.team2.teamName +"<br>";
-            console.log(element.leagueShortcut)
-        });
+checkGames();
 
-        // process your data further
-    } catch (error) {
-        console.error("Error:", error);
-    }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    checkGames("")
-})
 
 /*
   {
@@ -50,4 +53,47 @@ document.addEventListener("DOMContentLoaded", () => {
     "leagueSeason": "2023",
     "sport": null
   },
+*/
+
+/*
+<div class="flex flex-col items-center bg-blue-500 m-2 h-[80%] w-[20%] mb-10 mr-10">
+                <div>
+                    <p class="text-white text-center font-bold text-xs">
+                        24. November, 18:45
+                    </p>
+                </div>
+                <div>
+                    <p class="text-white text-center font-bold text-xs">
+                        Gruppe E
+                    </p>
+                </div>
+
+
+                <div class="flex items-center justify-between w-full">
+                    <div class="h-[3vh] w-[3vw] ml-[17%]"><a href="Teams.html">
+                        <img src="images/Teamlogos/lazio.png" alt="Lazio"></a>
+                    </div>
+                    <div class="text-4xl">
+                        <p class="text-white font-bold">
+                            :
+                        </p>
+                    </div>
+                    <div class="h-[3vh] w-[3vw] mr-[17%] mb-8"><a href="Teams.html">
+                        <img src="images/Teamlogos/celtic.png" alt="Celtic"></a>
+                    </div>
+                </div>
+                <div class="flex items-center justify-between w-full">
+                    <div class="text-center ml-[20%]">
+                        <p class="text-white align-text-bottom">
+                            Lazio
+                        </p>
+                    </div>
+                    <div class="text-center mr-[20%]">
+                        <p class="text-white align-text-bottom">
+                            Celtic
+                        </p>
+                    </div>
+                </div>
+            </div>
+
 */
