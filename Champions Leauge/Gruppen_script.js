15d8be7ca42febec4ab96542cc825f0da75a12c2
Florian <florian.gugele@gmail.com>
2023-12-02 11:29:34 +0100

    commit

---
+++ Champions Leauge/Gruppen_script.js
@@ -0,0 +1,107 @@
+function checkGroups() {
+
+    fetch('https://api.openligadb.de/getmatchdata/champion1/2023').then((response) =>
+        response.json().then((result) => {
+            fillGroups(result);
+        })
+    );
+}
+function fillGroups(data) {
+    console.log("data",data);
+        const element = document.getElementById("Gruppe");
+
+        let html = "";
+        data.forEach((Gruppe,index) => {
+            let postition = index + 1;
+            html += '<div class="h-[82vh] bg-contain w-full flex flex-wrap justify-center gap-10" id="Gruppe"><div class="relative mt-12"><div class="m-2 mb-2 bg-stone-200 z-10"><p class="font-bold">Gruppe A</p></div><div class="w-[15vw] h-[28vh] m-2 bg-stone-200 z-0 flex flex-col items-center justify-center"><div class="flex items-center justify-between bg-blue-500 m-2 h-12 mt-2 mb-0.5 w-[95%]"><p class="ml-2 mr-4">1</p>'
+            + postition +
+            '</p><a href="Teams.html"><img class="w-[3vh] h-[3vh] mx-2 mr-14" src="'
+            + Gruppe.teamIconUrl +
+            '</a><p class="mr-16">'
+            + Gruppe.teamName +
+            '</p><p class="mr-2">'
+            + Gruppe.points +
+            '</p></div>'
+
+
+            //zweiter platz
+
+            + '<div class="flex items-center justify-between bg-blue-500 m-2 h-12 mt-2 mb-0.5 w-[95%]"><p class="ml-2 mr-4">1</p>'
+            + postition +
+            '</p><a href="Teams.html"><img class="w-[3vh] h-[3vh] mx-2 mr-14" src="'
+            + Gruppe.teamIconUrl +
+            '</a><p class="mr-16">'
+            + Gruppe.teamName +
+            '</p><p class="mr-2">'
+            + Gruppe.points +
+            '</p></div>'
+
+
+            // dritter platz
+
+            +'<div class="flex items-center justify-between bg-blue-500 m-2 h-12 mt-2 mb-0.5 w-[95%]"><p class="ml-2 mr-4">1</p>'
+            + postition +
+            '</p><a href="Teams.html"><img class="w-[3vh] h-[3vh] mx-2 mr-14" src="'
+            + Gruppe.teamIconUrl +
+            '</a><p class="mr-16">'
+            + Gruppe.teamName +
+            '</p><p class="mr-2">'
+            + Gruppe.points +
+            '</p></div>'
+
+            // vierter platz
+
+            +'<div class="flex items-center justify-between bg-blue-500 m-2 h-12 mt-2 mb-0.5 w-[95%]"><p class="ml-2 mr-4">1</p>'
+            + postition +
+            '</p><a href="Teams.html"><img class="w-[3vh] h-[3vh] mx-2 mr-14" src="'
+            + Gruppe.teamIconUrl +
+            '</a><p class="mr-16">'
+            + Gruppe.teamName +
+            '</p><p class="mr-2">'
+            + Gruppe.points +
+            '</p></div></div></div>'
+    
+
+        });
+        document.getElementById("Gruppe").innerHTML = html;
+        console.log(html);
+
+    } 
+
+
+checkGroups();
+
+/*
+<div class="relative mt-12">
+            <div class="m-2 mb-2 bg-stone-200 z-10">
+                <p class="font-bold">Gruppe A</p>
+            </div>
+        
+            <div class="w-[15vw] h-[28vh] m-2 bg-stone-200 z-0 flex flex-col items-center justify-center">
+                <div class="flex items-center justify-between bg-blue-500 m-2 h-12 mt-2 mb-0.5 w-[95%]">
+                    <p class="ml-2 mr-4">1</p>
+                    <a href="Teams.html"><img class="w-[3vh] h-[3vh] mx-2 mr-14" src="images/Teamlogos/Bayern.png"></a>
+                    <p class="mr-16">Bayern</p>
+                    <p class="mr-2">12</p>
+                </div>
+                <div class="flex items-center justify-between bg-blue-500 m-2 h-12 mt-2 mb-0.5 w-[95%]">
+                    <p class="ml-2 mr-4">2</p>
+                    <a href="Teams.html"><img class="w-[3vh] h-[3vh] mx-2 mr-12" src="images/Teamlogos/Kopenhagen.png"></a>
+                    <p class="mr-10">Kopenhagen</p>
+                    <p class="mr-2">4</p>
+                </div>
+                <div class="flex items-center justify-between bg-blue-500 m-2 h-12 mt-2 mb-0.5 w-[95%]">
+                    <p class="ml-2 mr-4">3</p>
+                    <a href="Teams.html"><img class="w-[3vh] h-[3vh] mx-2 mr-10" src="images/Teamlogos/Gala.png"></a>
+                    <p class="mr-16">Galatasaray</p>
+                    <p class="mr-2">4</p>
+                </div>
+                <div class="flex items-center justify-between bg-blue-500 m-2 h-12 mt-2 mb-0.5 w-[95%]">
+                    <p class="ml-2 mr-4">4</p>
+                    <a href="Teams.html"><img class="w-[3vh] h-[3vh] mx-2 mr-10" src="images/Teamlogos/ManU.png"></a>
+                    <p class="mr-16">ManUnited</p>
+                    <p class="mr-2">3</p>
+                </div>
+            </div>
+        </div>
+*/
--- Champions Leauge/Team_script.js
+++ Champions Leauge/Team_script.js
@@ -5,37 +5,45 @@ function checkTeam() {
             fillTeam(result);
         })
     );
-    fetch('https://api.openligadb.de/getnextmatchbyleagueteam/4673/1201').then((response) =>
-        response.json().then((result) => {
-            fillTeam(result);
-        })
-    );
+
 }
 function fillTeam(data) {
-    console.log("data",data);
-        const element = document.getElementById("Teams");
+    console.log("data", data);
+    const element = document.getElementById("Teams");
 
-        let html1 = ' <div class="flex flex-col items-center bg-blue-500 m-2 h-[100%] mr-3 ml-5"><div class="flex items-center justify-between w-full mt-2"><div class="h-[3vh] w-[3vw] m-2">'
-            + Teams.team1.teamIconUrl + '</div><div class="text-xl"><p class="text-white font-bold">:</p></div><div class="h-[2.5vh] w-[2.5vw] m-2">'
-            + Teams.team2.teamIconUrl + '</div></div><div class="flex items-center justify-between w-full mt-10"><div class="text-center ml-[20%]"><p class="text-white align-text-bottom">'
-            + Teams.matchResults. + '</p></div><div class="text-center mr-[20%]"><p class="text-white align-text-bottom">'
-            + Teams.matchResults. + '</p></div></div></div>'
+    console.log("Hallo");
+    let html1 = '<div class="flex flex-col items-center bg-blue-500 m-2 h-[100%] mr-3 ml-5"><div class="flex items-center justify-between w-full mt-2"><div class="h-[3vh] w-[3vw] m-2">'
+        + Teams.team1.teamIconUrl + '</div><div class="text-xl"><p class="text-white font-bold">:</p></div><div class="h-[2.5vh] w-[2.5vw] m-2">'
+        + Teams.team2.teamIconUrl + '</div></div><div class="flex items-center justify-between w-full mt-10"><div class="text-center ml-[20%]"><p class="text-white align-text-bottom">'
+        + Teams.matchResults.pointsTeam1 + '</p></div><div class="text-center mr-[20%]"><p class="text-white align-text-bottom">'
+        + Teams.matchResults.pointsTeam2 + '</p></div></div></div>'
 
-        let html2 = '<div class="flex flex-col items-center bg-blue-500 m-2 h-[100%] mr-3 ml-5"><div class="flex items-center justify-between w-full mt-2"><div class="h-[3vh] w-[3vw] m-2">'
-            + Teams.team1.teamIconUrl + '</div><div class="text-xl"><p class="text-white font-bold">:</p></div><div class="h-[2.5vh] w-[2.5vw] m-2">'
-            + Teams.team2.teamIconUrl + '</div></div></div>'
-
-
-        document.getElementById("Teams").innerHTML = html1;
-        console.log(html1);
-
-        document.getElementById("Teams").innerHTML = html2;
-        console.log(html2);
-
-        // process your data further
-    } 
+    document.getElementById("Teams").innerHTML = html1;
+    console.log(html1);
+}
 
 
+
+/*
+fetch('https://api.openligadb.de/getnextmatchbyleagueteam/4673/1201').then((response) =>
+    response.json().then((result) => {
+        fillTeam(result);
+    })
+);
+function fillTeam(data) {
+    console.log("data", data);
+    const element = document.getElementById("Teams");
+    let html2 = '<div class="flex flex-col items-center bg-blue-500 m-2 h-[100%] mr-3 ml-5"><div class="flex items-center justify-between w-full mt-2"><div class="h-[3vh] w-[3vw] m-2">'
+        + Teams.team1.teamIconUrl + '</div><div class="text-xl"><p class="text-white font-bold">:</p></div><div class="h-[2.5vh] w-[2.5vw] m-2">'
+        + Teams.team2.teamIconUrl + '</div></div></div>'
+
+
+
+
+    document.getElementById("Teams").innerHTML = html2;
+    console.log(html2);
+}
+*/
 checkTeam();
 
 
